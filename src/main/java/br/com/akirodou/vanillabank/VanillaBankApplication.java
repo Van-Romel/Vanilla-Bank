910e94135ccd9c1493aff1b6a501cc78f6561c60
Van-Romel A S Neto <vanr.neto@hotmail.com>
2022-04-18 19:46:31 -0300

    Initial Commit

---
+++ src/main/java/br/com/akirodou/vanillabank/VanillaBankApplication.java
@@ -0,0 +1,13 @@
+package br.com.akirodou.vanillabank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VanillaBankApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VanillaBankApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/com/akirodou/vanillabank/VanillaBankApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.akirodou.vanillabank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VanillaBankApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
