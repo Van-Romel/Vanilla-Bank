90a50d547a46ba0a9acbade32d518fa7c4f4bc04
Van-Romel A S Neto <vanr.neto@hotmail.com>
2022-04-27 22:24:00 -0300

    Flyway implementado

---
+++ src/test/java/br/com/akirodou/vanillabank/VanillaBankApplicationTest.java
@@ -0,0 +1,12 @@
+package br.com.akirodou.vanillabank;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class VanillaBankApplicationTest {
+
+    @Test
+    void main() {
+    }
+}
