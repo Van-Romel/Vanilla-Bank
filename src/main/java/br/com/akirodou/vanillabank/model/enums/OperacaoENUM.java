0b51a44397a9011980a4c17f987aba479caa591b
unknown <g.sdomingues@hotmail.com>
2022-04-28 18:39:35 -0300

    Remocao da pasta enum

--- src/main/java/br/com/akirodou/vanillabank/model/enums/OperacaoENUM.java
+++
@@ -1,9 +0,0 @@
-package br.com.akirodou.vanillabank.model.enums;
-
-public class OperacaoENUM {
-
-    public static final String SAQUE = "Saque";
-    public static final String DEPOSITO = "Deposito";
-    public static final String TRANSFERENCIA = "Transferencia";
-
-}
