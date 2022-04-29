7d2b9d65cc542db659d04806d0bae7f14fa0d8a6
Van-Romel A S Neto <vanr.neto@hotmail.com>
2022-04-29 18:39:57 -0300

    testes atualizados

--- src/test/resources/migration/V1_0_1__create_cliente.sql
+++ src/test/resources/migration/V1_0_1__create_cliente.sql
@@ -1,10 +1,13 @@
 CREATE TABLE cliente
 (
     clt_id   BIGINT AUTO_INCREMENT NOT NULL,
-    clt_nome VARCHAR(255)          NULL,
-    clt_cpf  VARCHAR(255)          NULL,
+    clt_nome VARCHAR(255)          NOT NULL,
+    clt_cpf  VARCHAR(11)           NOT NULL,
     CONSTRAINT pk_cliente PRIMARY KEY (clt_id)
 );
+ALTER TABLE cliente
+    ENGINE = InnoDB;
+
 
 ALTER TABLE cliente
     ADD CONSTRAINT uc_cliente_clt_cpf UNIQUE (clt_cpf);
